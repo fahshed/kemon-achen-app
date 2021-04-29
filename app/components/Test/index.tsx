dcb67299fcd847be2769c86d04ad01fe95c84a3a
unknown <imshmen077@gmail.com>
2021-04-30 03:38:06 +0600

    husky place changed

--- app/components/Test/index.tsx
+++ app/components/Test/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 export default function Test() {
   return (

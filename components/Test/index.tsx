df2b3daa368388615001122740f375ebd3e62ed9
unknown <imshmen077@gmail.com>
2021-04-29 22:20:33 +0600

    Initial Commit

---
+++ components/Test/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+
+export default function Test() {
+  return (
+    <Pressable
+      style={{ backgroundColor: "pink", height: 30, width: 100 }}
+      android_ripple={{ radius: 0, color: "#eeeeee" }}
+      onPress={() => console.log("Pressed")}
+    >
+      <Text>Hello</Text>
+    </Pressable>
+  );
+}

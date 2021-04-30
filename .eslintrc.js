1c2e24f2a7bede0bb490b0ff7d42576a6222991d
unknown <imshmen077@gmail.com>
2021-04-30 06:08:51 +0600

    prettier fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'react/jsx-curly-newline': 0,
     'import/no-unresolved': 0,
     'import/prefer-default-export': 0,
+    'prettier/prettier': 0,
     'no-template-curly-in-string': 0,
     'no-confusing-arrow': 0,
     'dot-notation': 1,

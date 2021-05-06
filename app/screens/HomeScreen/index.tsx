b959f0d83e34420bd23b2c7e8add51038d6ac01b
unknown <imshmen077@gmail.com>
2021-05-06 07:30:28 +0600

    Data fetching sample added

--- app/screens/HomeScreen/index.tsx
+++ app/screens/HomeScreen/index.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
-
-import { Test, Sample } from '../../components';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 
+import { Sample } from '../../components';
+import { Body2Bold } from '../../styles';
+import Api from '../../api';
+
 export default function HomeScreen() {
+  const [loading, setLoading] = useState(true);
+  const [userInfo, setUserInfo] = useState({ name: '', role: '' });
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const credentials = {
+          email: 'as@gmail.com',
+          password: 'afsaf',
+        };
+        const response = await Api.loginUser(credentials);
+        setUserInfo({ name: response.name, role: response.role });
+        setLoading(false);
+        console.log('\nIn Home screen\n', response);
+      } catch (e) {
+        console.log('Error /loginUser', e);
+      }
+    })();
+  }, []);
+
   return (
-    <View>
-      <Test style={{ marginBottom: 16 }} />
+    <View style={{ padding: 8 }}>
       <Sample
         status="resolved"
         createdAt={new Date()}
         ticketType="Rick and Morty"
         description="I'm Mr Misix"
+        style={{ marginBottom: 16 }}
       />
+      {loading ? (
+        <Body2Bold>Loading...</Body2Bold>
+      ) : (
+        <>
+          <Body2Bold>Name: {userInfo.name}</Body2Bold>
+          <Body2Bold>Role: {userInfo.role}</Body2Bold>
+        </>
+      )}
     </View>
   );
 }

23e7c041a3bc61a1c15057ddcffefc8970170021
AJAkil <cyclosis47@gmail.com>
2021-07-19 02:43:00 +0600

    fixed passing id as param

--- app/screens/NearbyProfessionalScreen/index.tsx
+++ app/screens/NearbyProfessionalScreen/index.tsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList } from 'react-native';
 
 import ProfessionalCard from '../../components/ProfessionalCard';
-import { theme } from '../../config';
-import { ItemSeparator } from '../../components';
 import NavRoutes from '../../navigation/NavRoutes';
 import Api from '../../api';
 
@@ -11,7 +8,7 @@ function NearbyProfessionalScreen({ navigation }) {
   const [professionalList, setProfessionalList] = useState(null);
 
   const getProfessionalList = async () => {
-    const response = await Api.getSuggestedProfessionals();
+    const response = await Api.getSuggestedProfessionals('rank');
     setProfessionalList(response);
   };
 
@@ -19,145 +16,26 @@ function NearbyProfessionalScreen({ navigation }) {
     getProfessionalList();
   }, []);
 
-  //console.log(professionalList);
-
-  const suggestedProfessionals = [
-    {
-      userName: 'Dr Bilkis Rahman',
-      userId: '1234',
-      badges: [
-        {
-          name: 'PTSD',
-          id: '12345',
-        },
-        {
-          name: 'Anxiety',
-          id: '56789',
-        },
-      ],
-      rank: '100',
-      location: '24/B Baker Street',
-    },
-    {
-      userName: 'Dr Anisur Rahman',
-      userId: '1234d4',
-      badges: [
-        {
-          name: 'Stress',
-          id: '12345',
-        },
-        {
-          name: 'Anxiety',
-          id: '56789',
-        },
-      ],
-      rank: '100',
-      location: '24/B Baker Street',
-    },
-    {
-      userName: 'Dr Shahana',
-      userId: '1234sasasas4',
-      badges: [
-        {
-          name: 'PTSD',
-          id: '12345',
-        },
-        {
-          name: 'Anxiety',
-          id: '56789',
-        },
-      ],
-      rank: '100',
-      location: '24/B Baker Street',
-    },
-    {
-      userName: 'Dr Neil Armstrong',
-      userId: '12346',
-      badges: [
-        {
-          name: 'PTSD',
-          id: '12345',
-        },
-        {
-          name: 'Anxiety',
-          id: '56789',
-        },
-      ],
-      rank: '100',
-      location: '24/B Baker Street',
-    },
-    {
-      userName: 'Dr Rakin Rahman',
-      userId: '12347',
-      badges: [
-        {
-          name: 'PTSD',
-          id: '12345',
-        },
-        {
-          name: 'Anxiety',
-          id: '56789',
-        },
-      ],
-      rank: '100',
-      location: '24/B Baker Street',
-    },
-    {
-      userName: 'Mr Bilkis Rahman',
-      userId: '12348',
-      badges: [
-        {
-          name: 'PTSD',
-          id: '12345',
-        },
-        {
-          name: 'Anxiety',
-          id: '56789',
-        },
-      ],
-      rank: '100',
-      location: '24/B Baker Street',
-    },
-    {
-      userName: 'Mr Bilkis Rahman',
-      userId: '12348t',
-      badges: [
-        {
-          name: 'PTSD',
-          id: '12345',
-        },
-        {
-          name: 'Anxiety',
-          id: '56789',
-        },
-      ],
-      rank: '100',
-      location: '24/B Baker Street',
-    },
-  ];
-
-  const renderItem = ({ item }) => (
-    <ProfessionalCard
-      userName={item.userName}
-      // badges={item.badges}
-      rank={item.rank}
-      location={item.location}
-      handleNavigation={() =>
-        navigation.navigate(NavRoutes.PROFESSIONAL_USER_PROFILE)
-      }
-    />
-  );
+  console.log(professionalList);
 
   return (
     <>
-      <FlatList
-        data={suggestedProfessionals}
-        ItemSeparatorComponent={() => (
-          <ItemSeparator height={8} color={theme.grey1} />
-        )}
-        keyExtractor={(professional) => professional.userId}
-        renderItem={renderItem}
-      />
+      {professionalList &&
+        professionalList.map((professional, index) => (
+          <ProfessionalCard
+            key={index}
+            userName={professional.name}
+            badges={professional.specializations}
+            rank={professional.rank}
+            location={professional.address}
+            handleNavigation={() =>
+              navigation.navigate(
+                NavRoutes.PROFESSIONAL_USER_PROFILE,
+                professional._id,
+              )
+            }
+          />
+        ))}
     </>
   );
 }
